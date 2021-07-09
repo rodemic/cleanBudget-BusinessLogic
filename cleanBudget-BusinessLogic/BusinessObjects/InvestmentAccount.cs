9f8e23ed83f5808f68a2262a3a2caa23f67bd61f
Roderick Micallef <micallef.roderick1@gmail.com>
2021-07-09 14:00:17 +0200

    Added InvestmentAccount, improved financial account

--- cleanBudget-BusinessLogic/BusinessObjects/InvestmentAccount.cs
+++ cleanBudget-BusinessLogic/BusinessObjects/InvestmentAccount.cs
@@ -6,13 +6,12 @@ using System.Threading.Tasks;
 
 namespace cleanBudget_BL.BusinessObjects
 {
-    public class FinancialAccount
+    public class InvestmentAccount
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public double StartingAmount { get; set; }
         public DateTime SetDate { get; set; }
-        public virtual ICollection<Transaction> Transactions { get; set; }
+        public virtual ICollection<Investment> Investments { get; set; }
         public string AccountID { get; set; }
     }
 }
--- cleanBudget-BusinessLogic/BusinessObjects/Transaction.cs
+++ cleanBudget-BusinessLogic/BusinessObjects/Transaction.cs
@@ -11,6 +11,5 @@ namespace cleanBudget_BL.BusinessObjects
         public int ID { get; set; }
         public double Amount { get; set; }
         public Category Category { get; set; }
-        public string AccountID { get; set; }
     }
 }
