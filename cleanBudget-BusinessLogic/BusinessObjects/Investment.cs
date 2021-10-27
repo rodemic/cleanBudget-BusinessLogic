70dfc234c18788bcea3ce5168d080eae91484ca2
Roderick Micallef <micallef.roderick1@gmail.com>
2021-10-27 16:25:45 +0200

    BO updates

--- cleanBudget-BusinessLogic/BusinessObjects/Investment.cs
+++ cleanBudget-BusinessLogic/BusinessObjects/Investment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace cleanBudget_BL.BusinessObjects
     public class Investment
     {
         public int ID { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(4)]
         public string Ticker { get; set; }
         public double Amount { get; set; }
         public double Cost { get; set; }
--- cleanBudget-BusinessLogic/BusinessObjects/Transaction.cs
+++ cleanBudget-BusinessLogic/BusinessObjects/Transaction.cs
@@ -12,5 +12,6 @@ namespace cleanBudget_BL.BusinessObjects
         public double Amount { get; set; }
         public Category Category { get; set; }
         public int AccountID { get; set; }
+        public DateTime PurchaseDate { get; set; }
     }
 }
